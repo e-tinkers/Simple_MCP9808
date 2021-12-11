cf926c10ca4574a7ec1dfc865dc5b5cc8f3b1e0e
Henry Cheung <cheetah_henry@hotmail.com>
2021-12-11 15:30:35 +0800

    add with_alert example

---
+++ examples/with_alert/with_alert.ino
@@ -0,0 +1,51 @@
+#include <Simple_MCP9808.h>
+
+// To test the alert triggering, change the following parameters to suit your
+// temperature environment so that an alert can be obversed
+#define LOWER_LIMIT_TEMPERATURE -20
+#define UPPER_LIMIT_TEMPERATURE 25
+#define CRITICAL_TEMPERATURE 30
+
+Simple_MCP9808 mcp;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+
+  if (!mcp.begin()) {
+    Serial.println("Failed to communicate with MCP9808 sensor");
+    while (1) { delay(10); }
+  }
+  else {
+    // Setup temperature threshholds
+    mcp.setLowerTemperature(LOWER_LIMIT_TEMPERATURE);
+    mcp.setUpperTemperature(UPPER_LIMIT_TEMPERATURE);
+    mcp.setCriticalTemperature(CRITICAL_TEMPERATURE);
+  }
+}
+
+void loop() {
+
+  int16_t tempR = mcp.getTemperature();
+  Serial.print("Temperaure raw reading = ");
+  Serial.println(tempR);
+
+  float tempC = tempR/16.0;
+  Serial.print("Temperature in Celsius = ");
+  Serial.println(tempC);
+
+  float tempF = tempR/16.0 * 9.0 / 5.0 + 32;
+  Serial.print("Temperature in Fahrenheit = ");
+  Serial.println(tempF);
+
+  int16_t alertStatus = mcp.alertTriggered();
+
+  if (alertStatus & LOWER_TEMP_ALERT)
+    Serial.println("*** Temperature < Preset Lower Limit");
+  if (alertStatus & UPPER_TEMP_ALERT)
+    Serial.println("*** Temperature > Preset Upper Limited");
+  if (alertStatus & CRITICAL_TEMP_ALERT)
+    Serial.println("*** Temperature > Preset Critial Temperature");
+
+  delay(5000);
+}
